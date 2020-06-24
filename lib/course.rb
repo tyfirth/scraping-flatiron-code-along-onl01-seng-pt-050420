70f53ae7aaa21587ae66ea61cd733c0455da3581
Tyler Firth <tyfirth@gmail.com>
2020-06-24 02:08:52 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Course
   
     attr_accessor :title, :schedule, :description
@@ -20,3 +21,24 @@ class Course
 
   
 
+=======
+class Course
+  
+  attr_accessor :title, :schedule, :description
+  
+  @@all = []
+  
+  def initialize
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+end
+>>>>>>> 2236ecc64ef25ff2c8354aefab91f3c03b0acaa0
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
@@ -40,3 +41,36 @@ end
 Scraper.new.get_page
 Scraper.new.print_courses
 
+=======
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+require_relative './course.rb'
+
+class Scraper
+  
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    
+        binding.pry
+  end
+  
+  
+  
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+  
+end
+
+Scraper.new.get_page
+
+>>>>>>> 2236ecc64ef25ff2c8354aefab91f3c03b0acaa0
